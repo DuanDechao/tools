6199fe5b986d0c44f8c1f58acd9812107270cc35
duandechao <ddc_cug@163.com>
2020-02-21 01:09:41 +0800

    dfas

--- init_env.sh
+++ init_env.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 work_dir=$(cd `dirname $0`; pwd)
-cp {work_dir}/.bashrc /root/
+echo ${work_dir}
+cp ${work_dir}/.bashrc /root/
 source /root/.bashrc
 
 mkdir -p /root/.vim/bundle
-cp {work_dir}/comm.vim /root/.vim/
+cp ${work_dir}/comm.vim /root/.vim/
 git clone https://github.com/VundleVim/Vundle.vim.git /root/.vim/bundle/vundle
 touch /root/.vimrc
 echo "source /root/.vim/comm.vim" >>  /root/.vimrc
